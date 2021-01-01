f79d1d260a1d9d5c6ac9961318dc0f1686cedf09
Auxilor <will@willfp.com>
2021-01-01 20:17:24 +0000

    Updated existing talismans to work with ranged attacks

--- eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/talismans/SkeletonTalisman.java
+++ eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/talismans/SkeletonTalisman.java
@@ -2,9 +2,11 @@ package com.willfp.talismans.talismans.talismans;
 
 import com.willfp.talismans.talismans.Talisman;
 import com.willfp.talismans.talismans.Talismans;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Trident;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,6 +19,30 @@ public class SkeletonTalisman extends Talisman {
     public void onMeleeAttack(@NotNull final Player attacker,
                               @NotNull final LivingEntity victim,
                               @NotNull final EntityDamageByEntityEvent event) {
+        if (!(victim instanceof Skeleton)) {
+            return;
+        }
+
+        event.setDamage(event.getDamage() * (1 + (this.getConfig().getDouble(Talismans.CONFIG_LOCATION + "percent-more-damage")) / 100));
+    }
+
+    @Override
+    public void onArrowDamage(@NotNull final Player attacker,
+                              @NotNull final LivingEntity victim,
+                              @NotNull final Arrow arrow,
+                              @NotNull final EntityDamageByEntityEvent event) {
+        if (!(victim instanceof Skeleton)) {
+            return;
+        }
+
+        event.setDamage(event.getDamage() * (1 + (this.getConfig().getDouble(Talismans.CONFIG_LOCATION + "percent-more-damage")) / 100));
+    }
+
+    @Override
+    public void onTridentDamage(@NotNull final Player attacker,
+                                @NotNull final LivingEntity victim,
+                                @NotNull final Trident trident,
+                                @NotNull final EntityDamageByEntityEvent event) {
         if (!(victim instanceof Skeleton)) {
             return;
         }
--- eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/talismans/ZombieTalisman.java
+++ eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/talismans/ZombieTalisman.java
@@ -2,8 +2,10 @@ package com.willfp.talismans.talismans.talismans;
 
 import com.willfp.talismans.talismans.Talisman;
 import com.willfp.talismans.talismans.Talismans;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Trident;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.jetbrains.annotations.NotNull;
@@ -17,6 +19,30 @@ public class ZombieTalisman extends Talisman {
     public void onMeleeAttack(@NotNull final Player attacker,
                               @NotNull final LivingEntity victim,
                               @NotNull final EntityDamageByEntityEvent event) {
+        if (!(victim instanceof Zombie)) {
+            return;
+        }
+
+        event.setDamage(event.getDamage() * (1 + (this.getConfig().getDouble(Talismans.CONFIG_LOCATION + "percent-more-damage")) / 100));
+    }
+
+    @Override
+    public void onArrowDamage(@NotNull final Player attacker,
+                              @NotNull final LivingEntity victim,
+                              @NotNull final Arrow arrow,
+                              @NotNull final EntityDamageByEntityEvent event) {
+        if (!(victim instanceof Zombie)) {
+            return;
+        }
+
+        event.setDamage(event.getDamage() * (1 + (this.getConfig().getDouble(Talismans.CONFIG_LOCATION + "percent-more-damage")) / 100));
+    }
+
+    @Override
+    public void onTridentDamage(@NotNull final Player attacker,
+                                @NotNull final LivingEntity victim,
+                                @NotNull final Trident trident,
+                                @NotNull final EntityDamageByEntityEvent event) {
         if (!(victim instanceof Zombie)) {
             return;
         }
