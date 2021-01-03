1d082e8b7220f4b3f837997b42f8bfb4ee9b90db
Auxilor <will@willfp.com>
2021-01-03 19:35:03 +0000

    Added healing talisman/ring/relic

--- eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/Talismans.java
+++ eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/Talismans.java
@@ -13,6 +13,7 @@ import com.willfp.talismans.talismans.talismans.relic.ExtractionRelic;
 import com.willfp.talismans.talismans.talismans.relic.FeatherRelic;
 import com.willfp.talismans.talismans.talismans.relic.FlameRelic;
 import com.willfp.talismans.talismans.talismans.relic.FluxRelic;
+import com.willfp.talismans.talismans.talismans.relic.HealingRelic;
 import com.willfp.talismans.talismans.talismans.relic.NetherRelic;
 import com.willfp.talismans.talismans.talismans.relic.PoseidonRelic;
 import com.willfp.talismans.talismans.talismans.relic.RaidRelic;
@@ -27,6 +28,7 @@ import com.willfp.talismans.talismans.talismans.ring.ExtractionRing;
 import com.willfp.talismans.talismans.talismans.ring.FeatherRing;
 import com.willfp.talismans.talismans.talismans.ring.FlameRing;
 import com.willfp.talismans.talismans.talismans.ring.FluxRing;
+import com.willfp.talismans.talismans.talismans.ring.HealingRing;
 import com.willfp.talismans.talismans.talismans.ring.NetherRing;
 import com.willfp.talismans.talismans.talismans.ring.PoseidonRing;
 import com.willfp.talismans.talismans.talismans.ring.RaidRing;
@@ -42,6 +44,7 @@ import com.willfp.talismans.talismans.talismans.talisman.ExtractionTalisman;
 import com.willfp.talismans.talismans.talismans.talisman.FeatherTalisman;
 import com.willfp.talismans.talismans.talismans.talisman.FlameTalisman;
 import com.willfp.talismans.talismans.talismans.talisman.FluxTalisman;
+import com.willfp.talismans.talismans.talismans.talisman.HealingTalisman;
 import com.willfp.talismans.talismans.talismans.talisman.ImmunityTalisman;
 import com.willfp.talismans.talismans.talismans.talisman.NetherTalisman;
 import com.willfp.talismans.talismans.talismans.talisman.PoseidonTalisman;
@@ -121,6 +124,9 @@ public class Talismans {
     public static final Talisman FLUX_TALISMAN = new FluxTalisman();
     public static final Talisman FLUX_RING = new FluxRing();
     public static final Talisman FLUX_RELIC = new FluxRelic();
+    public static final Talisman HEALING_TALISMAN = new HealingTalisman();
+    public static final Talisman HEALING_RING = new HealingRing();
+    public static final Talisman HEALING_RELIC = new HealingRelic();
 
 
     /**
---
+++ eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/talismans/ring/HealingRing.java
@@ -0,0 +1,42 @@
+package com.willfp.talismans.talismans.talismans.ring;
+
+import com.willfp.talismans.talismans.Talisman;
+import com.willfp.talismans.talismans.Talismans;
+import com.willfp.talismans.talismans.meta.TalismanStrength;
+import com.willfp.talismans.talismans.util.TalismanChecks;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class HealingRing extends Talisman {
+    public HealingRing() {
+        super("healing_ring", TalismanStrength.RING);
+    }
+
+    @EventHandler
+    public void onHeal(@NotNull final EntityRegainHealthEvent event) {
+        if (!(event.getEntity() instanceof Player)) {
+            return;
+        }
+
+        if (!event.getRegainReason().equals(EntityRegainHealthEvent.RegainReason.SATIATED) && !event.getRegainReason().equals(EntityRegainHealthEvent.RegainReason.REGEN)) {
+            return;
+        }
+
+        Player player = (Player) event.getEntity();
+
+        if (this.getDisabledWorlds().contains(player.getWorld())) {
+            return;
+        }
+
+        if (!TalismanChecks.hasTalisman(player, this)) {
+            return;
+        }
+
+        double amount = this.getConfig().getDouble(Talismans.CONFIG_LOCATION + "multiplier");
+        amount += 1;
+
+        event.setAmount(event.getAmount() * amount);
+    }
+}
