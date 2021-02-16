ca4d140a1464d809a591f94748604c76309f21de
Auxilor <will@willfp.com>
2021-02-16 15:10:15 +0000

    Removed redundant TalismanYamlConfig

--- eco-core/core-plugin/src/main/java/com/willfp/talismans/config/configs/TalismanConfig.java
+++ eco-core/core-plugin/src/main/java/com/willfp/talismans/config/configs/TalismanConfig.java
@@ -1,11 +1,12 @@
 package com.willfp.talismans.config.configs;
 
-import com.willfp.talismans.config.TalismanYamlConfig;
+import com.willfp.eco.util.config.ExtendableConfig;
+import com.willfp.talismans.TalismansPlugin;
 import com.willfp.talismans.talismans.meta.TalismanStrength;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
-public class TalismanConfig extends TalismanYamlConfig {
+public class TalismanConfig extends ExtendableConfig {
     /**
      * The name of the config.
      */
@@ -17,12 +18,12 @@ public class TalismanConfig extends TalismanYamlConfig {
      *
      * @param name     The name of the config.
      * @param strength The strength of the talisman.
-     * @param plugin   The provider of the talisman.
+     * @param source   The provider of the talisman.
      */
     public TalismanConfig(@NotNull final String name,
                           @NotNull final TalismanStrength strength,
-                          @NotNull final Class<?> plugin) {
-        super(name, strength, plugin);
+                          @NotNull final Class<?> source) {
+        super(name, true, TalismansPlugin.getInstance(), source, "talismans/" + strength.name().toLowerCase() + "/");
         this.name = name;
     }
 }
