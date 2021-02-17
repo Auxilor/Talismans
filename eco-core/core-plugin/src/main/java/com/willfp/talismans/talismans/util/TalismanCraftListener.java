e8f19400ce7a0a2631a6421c92dd8268d76dd201
Auxilor <will@willfp.com>
2021-02-17 15:39:35 +0000

    Removed redundant listeners

--- eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/util/TalismanCraftListener.java
+++ eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/util/TalismanCraftListener.java
@@ -2,9 +2,6 @@ package com.willfp.talismans.talismans.util;
 
 import com.willfp.eco.util.internal.PluginDependent;
 import com.willfp.eco.util.plugin.AbstractEcoPlugin;
-import com.willfp.eco.util.recipe.parts.RecipePart;
-import com.willfp.eco.util.recipe.parts.SimpleRecipePart;
-import com.willfp.eco.util.recipe.recipes.EcoShapedRecipe;
 import com.willfp.talismans.talismans.Talisman;
 import com.willfp.talismans.talismans.Talismans;
 import org.bukkit.Material;
@@ -68,64 +65,6 @@ public class TalismanCraftListener extends PluginDependent implements Listener {
      * @param event The event to listen for.
      */
     @EventHandler
-    public void preventUsingTalismanInTalCraft(@NotNull final PrepareItemCraftEvent event) {
-        if (!(event.getRecipe() instanceof ShapedRecipe)) {
-            return;
-        }
-
-        ShapedRecipe recipe = (ShapedRecipe) event.getRecipe();
-
-        Talisman talisman = Talismans.getByKey(recipe.getKey());
-
-        if (talisman == null) {
-            return;
-        }
-
-        EcoShapedRecipe ecoShapedRecipe = talisman.getRecipe();
-
-        for (int i = 0; i < 9; i++) {
-            ItemStack itemStack = event.getInventory().getMatrix()[i];
-            RecipePart part = ecoShapedRecipe.getParts()[i];
-            if (part instanceof SimpleRecipePart) {
-                if (TalismanChecks.getTalismanOnItem(itemStack) != null) {
-                    event.getInventory().setResult(new ItemStack(Material.AIR));
-                    return;
-                }
-            }
-        }
-    }
-
-    /**
-     * Prevents using talismans in recipes.
-     *
-     * @param event The event to listen for.
-     */
-    @EventHandler
-    public void preventUseTalismanInCraft(@NotNull final PrepareItemCraftEvent event) {
-        if (!(event.getRecipe() instanceof ShapedRecipe)) {
-            return;
-        }
-
-        ShapedRecipe recipe = (ShapedRecipe) event.getRecipe();
-
-        if (AbstractEcoPlugin.LOADED_ECO_PLUGINS.contains(recipe.getKey().getNamespace())) {
-            return;
-        }
-
-        for (ItemStack itemStack : event.getInventory().getMatrix()) {
-            if (TalismanChecks.getTalismanOnItem(itemStack) != null) {
-                event.getInventory().setResult(new ItemStack(Material.AIR));
-                return;
-            }
-        }
-    }
-
-    /**
-     * Called on item craft.
-     *
-     * @param event The event to listen for.
-     */
-    @EventHandler
     public void onCraft(@NotNull final CraftItemEvent event) {
         if (!(event.getRecipe() instanceof ShapedRecipe)) {
             return;
@@ -152,66 +91,6 @@ public class TalismanCraftListener extends PluginDependent implements Listener {
         if (!player.hasPermission(permission)) {
             event.getInventory().setResult(new ItemStack(Material.AIR));
             event.setCancelled(true);
-        }
-    }
-
-    /**
-     * Prevents using talismans in recipes.
-     *
-     * @param event The event to listen for.
-     */
-    @EventHandler
-    public void preventUseTalismanInCraft(@NotNull final CraftItemEvent event) {
-        if (!(event.getRecipe() instanceof ShapedRecipe)) {
-            return;
-        }
-
-        ShapedRecipe recipe = (ShapedRecipe) event.getRecipe();
-
-        if (AbstractEcoPlugin.LOADED_ECO_PLUGINS.contains(recipe.getKey().getNamespace())) {
-            return;
-        }
-
-        for (ItemStack itemStack : event.getInventory().getMatrix()) {
-            if (TalismanChecks.getTalismanOnItem(itemStack) != null) {
-                event.getInventory().setResult(new ItemStack(Material.AIR));
-                event.setCancelled(true);
-                return;
-            }
-        }
-    }
-
-    /**
-     * Called on item craft.
-     *
-     * @param event The event to listen for.
-     */
-    @EventHandler
-    public void preventUsingTalismanInTalCraft(@NotNull final CraftItemEvent event) {
-        if (!(event.getRecipe() instanceof ShapedRecipe)) {
-            return;
-        }
-
-        ShapedRecipe recipe = (ShapedRecipe) event.getRecipe();
-
-        Talisman talisman = Talismans.getByKey(recipe.getKey());
-
-        if (talisman == null) {
-            return;
-        }
-
-        EcoShapedRecipe ecoShapedRecipe = talisman.getRecipe();
-
-        for (int i = 0; i < 9; i++) {
-            ItemStack itemStack = event.getInventory().getMatrix()[i];
-            RecipePart part = ecoShapedRecipe.getParts()[i];
-            if (part instanceof SimpleRecipePart) {
-                if (TalismanChecks.getTalismanOnItem(itemStack) != null) {
-                    event.getInventory().setResult(new ItemStack(Material.AIR));
-                    event.setCancelled(true);
-                    return;
-                }
-            }
         }
     }
 }
