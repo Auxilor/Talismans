d092e656716b1effe1653e11d0bd42f1fc5f94e4
Auxilor <will@willfp.com>
2021-03-14 20:48:24 +0000

    Fixed boss talisman with ecobosses

--- eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/talismans/BossTalisman.java
+++ eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/talismans/BossTalisman.java
@@ -3,15 +3,18 @@ package com.willfp.talismans.talismans.talismans;
 import com.willfp.talismans.talismans.Talisman;
 import com.willfp.talismans.talismans.Talismans;
 import com.willfp.talismans.talismans.meta.TalismanStrength;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Boss;
-import org.bukkit.entity.Illusioner;
+import org.bukkit.entity.ElderGuardian;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Trident;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
+@SuppressWarnings("deprecation")
 public class BossTalisman extends Talisman {
     public BossTalisman(@NotNull final TalismanStrength strength) {
         super("boss", strength);
@@ -21,7 +24,7 @@ public class BossTalisman extends Talisman {
     public void onMeleeAttack(@NotNull final Player attacker,
                               @NotNull final LivingEntity victim,
                               @NotNull final EntityDamageByEntityEvent event) {
-        if (!(victim instanceof Boss || victim instanceof Illusioner)) {
+        if (!(victim instanceof Boss || victim instanceof ElderGuardian) && !victim.getPersistentDataContainer().has(new NamespacedKey("ecobosses", "boss"), PersistentDataType.STRING)) {
             return;
         }
 
@@ -33,7 +36,7 @@ public class BossTalisman extends Talisman {
                               @NotNull final LivingEntity victim,
                               @NotNull final Arrow arrow,
                               @NotNull final EntityDamageByEntityEvent event) {
-        if (!(victim instanceof Boss || victim instanceof Illusioner)) {
+        if (!(victim instanceof Boss || victim instanceof ElderGuardian) && !victim.getPersistentDataContainer().has(new NamespacedKey("ecobosses", "boss"), PersistentDataType.STRING)) {
             return;
         }
 
@@ -45,7 +48,7 @@ public class BossTalisman extends Talisman {
                                 @NotNull final LivingEntity victim,
                                 @NotNull final Trident trident,
                                 @NotNull final EntityDamageByEntityEvent event) {
-        if (!(victim instanceof Boss || victim instanceof Illusioner)) {
+        if (!(victim instanceof Boss || victim instanceof ElderGuardian) && !victim.getPersistentDataContainer().has(new NamespacedKey("ecobosses", "boss"), PersistentDataType.STRING)) {
             return;
         }
 
