498b6914798b7f81c9427562d09ea6e08523c3e1
Auxilor <william.favierparsons1@gmail.com>
2021-06-14 20:54:44 +0100

    Reduced duplicate code

--- eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/talismans/StrengthTalisman.java
+++ eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/talismans/StrengthTalisman.java
@@ -3,11 +3,9 @@ package com.willfp.talismans.talismans.talismans;
 import com.willfp.talismans.talismans.Talisman;
 import com.willfp.talismans.talismans.TalismanLevel;
 import com.willfp.talismans.talismans.Talismans;
-import org.bukkit.entity.Arrow;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Trident;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class StrengthTalisman extends Talisman {
@@ -16,28 +14,10 @@ public class StrengthTalisman extends Talisman {
     }
 
     @Override
-    public void onMeleeAttack(@NotNull final TalismanLevel level,
-                              @NotNull final Player attacker,
-                              @NotNull final LivingEntity victim,
-                              @NotNull final EntityDamageByEntityEvent event) {
-        event.setDamage(event.getDamage() * (1 + (level.getConfig().getDouble(Talismans.CONFIG_LOCATION + "percent-more-damage")) / 100));
-    }
-
-    @Override
-    public void onArrowDamage(@NotNull final TalismanLevel level,
-                              @NotNull final Player attacker,
-                              @NotNull final LivingEntity victim,
-                              @NotNull final Arrow arrow,
-                              @NotNull final EntityDamageByEntityEvent event) {
-        event.setDamage(event.getDamage() * (1 + (level.getConfig().getDouble(Talismans.CONFIG_LOCATION + "percent-more-damage")) / 100));
-    }
-
-    @Override
-    public void onTridentDamage(@NotNull final TalismanLevel level,
-                                @NotNull final Player attacker,
-                                @NotNull final LivingEntity victim,
-                                @NotNull final Trident trident,
-                                @NotNull final EntityDamageByEntityEvent event) {
+    public void onAnyAttack(@NotNull final TalismanLevel level,
+                            @NotNull final Player attacker,
+                            @NotNull final LivingEntity victim,
+                            @NotNull final EntityDamageEvent event) {
         event.setDamage(event.getDamage() * (1 + (level.getConfig().getDouble(Talismans.CONFIG_LOCATION + "percent-more-damage")) / 100));
     }
 }
