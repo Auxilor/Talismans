7b7748c90fd99f8daddab0bbfa268a68905ea8a9
Auxilor <william.favierparsons1@gmail.com>
2021-11-12 10:49:52 +0000

    Added guardian talisman

---
+++ eco-core/core-plugin/src/main/java/com/willfp/talismans/talismans/talismans/GuardianTalisman.java
@@ -0,0 +1,30 @@
+package com.willfp.talismans.talismans.talismans;
+
+import com.willfp.talismans.talismans.Talisman;
+import com.willfp.talismans.talismans.TalismanLevel;
+import com.willfp.talismans.talismans.Talismans;
+import org.bukkit.entity.ElderGuardian;
+import org.bukkit.entity.Guardian;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class GuardianTalisman extends Talisman {
+    public GuardianTalisman() {
+    super("guardian");
+    }
+
+    @Override
+    public void onAnyAttack(@NotNull final TalismanLevel level,
+                            @NotNull final Player attacker,
+                            @NotNull final LivingEntity victim,
+                            @NotNull final EntityDamageEvent event) {
+        if (!(victim instanceof Guardian)) {
+            return;
+        }
+
+        event.setDamage(event.getDamage() * (1 + (level.getConfig().getDouble(Talismans.CONFIG_LOCATION + "percent-more-damage")) / 100));
+    }
+}
