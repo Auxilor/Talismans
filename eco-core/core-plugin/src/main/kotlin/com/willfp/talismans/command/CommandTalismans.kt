19e79c10807bb5f25c6915840d4f3295190266dd
Auxilor <william.favierparsons1@gmail.com>
2022-10-13 20:13:06 +0100

    libreforge-updater

--- eco-core/core-plugin/src/main/kotlin/com/willfp/talismans/command/CommandTalismans.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/talismans/command/CommandTalismans.kt
@@ -2,14 +2,28 @@ package com.willfp.talismans.command
 
 import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.command.impl.PluginCommand
+import com.willfp.libreforge.LibReforgePlugin
+import com.willfp.libreforge.lrcdb.CommandExport
+import com.willfp.libreforge.lrcdb.CommandImport
+import com.willfp.libreforge.lrcdb.ExportableConfig
+import com.willfp.talismans.talismans.Talismans
 import org.bukkit.command.CommandSender
 
-class CommandTalismans(plugin: EcoPlugin)
+class CommandTalismans(plugin: LibReforgePlugin)
     : PluginCommand(plugin, "talismans", "talismans.command.talismans", false) {
     init {
         addSubcommand(CommandReload(plugin))
             .addSubcommand(CommandGive(plugin))
             .addSubcommand(CommandBag(plugin))
+            .addSubcommand(CommandImport("talismans", plugin))
+            .addSubcommand(CommandExport(plugin) {
+                Talismans.values().map {
+                    ExportableConfig(
+                        it.id,
+                        it.config
+                    )
+                }
+            })
     }
 
     override fun onExecute(sender: CommandSender, args: List<String>) {
