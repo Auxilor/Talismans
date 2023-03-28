83c62bec2230ec8366d86ea6f625d8a3edbba55a
Auxilor <william.favierparsons1@gmail.com>
2023-03-28 14:53:15 +0100

    Fixed publication

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -10,6 +10,7 @@ publishing {
     publications {
         register("maven", MavenPublication::class) {
             from(components["java"])
+            artifactId = rootProject.name
         }
     }
 }
