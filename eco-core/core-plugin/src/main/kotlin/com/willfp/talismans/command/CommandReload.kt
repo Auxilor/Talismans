da1aaff5113d175c40a18531b1fb77f86b2b10fc
NicoNeko <nicuch100@gmail.com>
2026-03-09 08:34:40 +0200

    Relocate kotlin Add %time% to reload

--- eco-core/core-plugin/src/main/kotlin/com/willfp/talismans/command/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/talismans/command/CommandReload.kt
@@ -1,6 +1,7 @@
 package com.willfp.talismans.command
 
 import com.willfp.eco.core.command.impl.Subcommand
+import com.willfp.eco.util.StringUtils
 import com.willfp.talismans.plugin
 import org.bukkit.command.CommandSender
 
@@ -12,8 +13,8 @@ object CommandReload : Subcommand(
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
         sender.sendMessage(
-            plugin.langYml.getMessage("reloaded")
-                .replace("%time%", plugin.reloadWithTime().toString() + "")
+            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                .replace("%time%", plugin.reloadWithTime().toString())
         )
     }
 }
